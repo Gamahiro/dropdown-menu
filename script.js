2e46e8e6d17deb84486743eb3926f2c2f580543e
Johan <jpettersen50@gmail.com>
2022-12-02 02:12:36 +0100

    s

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-const dropdownBtn = document.querySelector('#menuContainer');
 
 
-
-for (let index = 0; index < dropdownBtn.childElementCount; index++) {
-
-    dropdownBtn.children[index].addEventListener('click', () => {
-        dropdownToggle(dropdownBtn.children[index]);
-    });
-}
-
-document.querySelector('#menuBtn3').addEventListener('click', (e) => {
+/* document.querySelector('#menuBtn1').addEventListener('click', (e) => {
     //prevents child elements to trigger this event
     if(e.target !== e.currentTarget) {
         return;
     }
 
-    dropdownToggle(document.querySelector('#menuBtn3'));
-});
+    dropdownToggle(document.querySelector('#menuContainer1'));
+}); */
+
+createDropDownMenu(document.querySelector('#menuBtn1'), document.querySelector('#menuContainer1'));
+createDropDownMenu(document.querySelector('#menuBtn2'), document.querySelector('#menuContainer2'));
+createDropDownMenu(document.querySelector('#menuBtn3'), document.querySelector('#menuContainer3'));
+
+function createDropDownMenu (btn, menuContainer) {
+    btn.addEventListener('click', (e) => {
+
+        if(e.target !== e.currentTarget) {
+            return;
+        }
+
+        dropdownToggle(menuContainer);
+    });
+}
 
    //takes an argument DOM element and sets visibility to visible if hidden, else hidden
    //element should have visibility set to hidden as default
 function dropdownToggle(toggleElement) {
-    for (let i = 0; i < toggleElement.childElementCount; i++) {
-            if(toggleElement.children[i].style.visibility !== 'visible') {
-                toggleElement.children[i].style.visibility = 'visible';
-                console.log('visible');
-            }else {
-                toggleElement.children[i].style.visibility = 'hidden';
-                console.log('hidden');
-            }
+    console.log(toggleElement)
+
+    if(toggleElement.style.display !== 'block') {
+        toggleElement.style.display = 'block';
     }
+    else {
+        toggleElement.style.display = 'none';
+    }
+
+    /* for (let i = 0; i < toggleElement.childElementCount; i++) {
+            if(toggleElement.children[i].style.display !== 'block') {
+                toggleElement.children[i].style.display = 'block';
+                console.log('block');
+            }else {
+                toggleElement.children[i].style.display = 'none';
+                console.log('none');
+            }
+    } */
 }
 
 
